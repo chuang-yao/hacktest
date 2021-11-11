20906b78ffe53128fa3a957c9e1bb26498836af3
Chuang Yao <chuang.calvin.yao@gmail.com>
2021-11-11 00:00:03 -0500

    added the ExecutionHandler class

---
+++ ht/ExecutionHandler/ExecutionHandler.hpp
@@ -0,0 +1,14 @@
+//
+// Created by chuan on 11/10/2021.
+//
+
+#ifndef HACKTEST_EXECUTIONHANDLER_HPP
+#define HACKTEST_EXECUTIONHANDLER_HPP
+
+namespace HackTest {
+
+class ExecutionHandler {};
+
+} // namespace HackTest
+
+#endif // HACKTEST_EXECUTIONHANDLER_HPP
---
+++ ht/ExecutionHandler/all.hpp
@@ -0,0 +1,10 @@
+//
+// Created by chuan on 11/10/2021.
+//
+
+#ifndef HACKTEST_ALL_HPP
+#define HACKTEST_ALL_HPP
+
+#include "ht/ExecutionHandler/ExecutionHandler.hpp"
+
+#endif // HACKTEST_ALL_HPP
