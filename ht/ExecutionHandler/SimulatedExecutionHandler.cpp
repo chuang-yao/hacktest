3a2bed161239f14f2bd2098fe4253962f00d8f42
Chuang Yao <chuang.calvin.yao@gmail.com>
2021-11-25 12:29:01 -0500

    changed `#include` to full path

--- ht/ExecutionHandler/SimulatedExecutionHandler.cpp
+++ ht/ExecutionHandler/SimulatedExecutionHandler.cpp
@@ -2,7 +2,7 @@
 // Created by Chuang Yao on 11/25/21.
 //
 
-#include "SimulatedExecutionHandler.hpp"
+#include "ht/ExecutionHandler/SimulatedExecutionHandler.hpp"
 
 namespace HackTest {
 
--- ht/Strategy/SimpleStrategy.cpp
+++ ht/Strategy/SimpleStrategy.cpp
@@ -2,7 +2,7 @@
 // Created by Chuang Yao on 11/25/21.
 //
 
-#include "SimpleStrategy.hpp"
+#include "ht/Strategy/SimpleStrategy.hpp"
 
 namespace HackTest {
 
