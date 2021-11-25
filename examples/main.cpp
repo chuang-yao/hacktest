a0896f1d788ea70b123114cb3429c9a0fb463629
Chuang Yao <chuang.calvin.yao@gmail.com>
2021-11-24 20:10:40 -0500

    renamed main program to `HackTest`

--- examples/main.cpp
+++ examples/main.cpp
@@ -5,5 +5,31 @@
 // Driver program for HackTest
 
 #include "ht/Event/Event.hpp"
+#include "ht/DataHandler/HistoricalCsvHandler.hpp"
+#include "ht/Strategy/Strategy.hpp"
+#include "ht/Portfolio/SimplePortfolio.hpp"
+#include "ht/ExecutionHandler/ExecutionHandler.hpp"
 
-int main() { HackTest::EventQueue q; }
+#include <vector>
+#include <string>
+
+namespace fs = std::filesystem;
+
+int main() {
+  std::vector<std::string> symbols{"AAPL", "^IXIC", "MSFT"};
+#ifdef __linux__
+  fs::path path{"/home/chuang/HackTest/examples/data"};
+#endif
+#ifdef _WIN32
+  fs::path path{R"(C:\Users\Chuang\CLionProjects\HackTest\examples\data)"};
+#endif
+#ifdef __APPLE__
+  fs::path path{"/Users/cyao/Developer/CLionProjects/HackTest/examples/data"};
+#endif
+
+  HackTest::EventQueue q;
+  HackTest::HistoricalCsvHandler bars(q, path, symbols);
+  HackTest::Strategy strat;
+  HackTest::SimplePortfolio port;
+  HackTest::ExecutionHandler broker;
+}
