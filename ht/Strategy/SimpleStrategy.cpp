fe5f736b14e1873b16d67a1a8bee6b10e013f3cb
Chuang Yao <chuang.calvin.yao@gmail.com>
2021-11-26 21:53:33 -0500

    added declaration and definition for `SimpleStrategy`

--- ht/Strategy/SimpleStrategy.cpp
+++ ht/Strategy/SimpleStrategy.cpp
@@ -3,9 +3,24 @@
 //
 
 #include "ht/Strategy/SimpleStrategy.hpp"
+#include "ht/Event/SignalEvent.hpp"
 
 namespace HackTest {
 
-void SimpleStrategy::calculate_signal(MarketEvent &) {}
+SimpleStrategy::SimpleStrategy(EventQueue &q, HistoricalCsvHandler &dh)
+    : q_(q), dh_(dh), symbols_(dh_.symbols_) {
+  for (const auto &symbol : symbols_) {
+    bought_.insert({symbol, false});
+  }
+}
+
+void SimpleStrategy::calculate_signal(MarketEvent &me) {
+  for (const auto &symbol : symbols_) {
+    if (!dh_.latest_data_.empty()) {
+      SignalEvent se(q_, dh_.latest_data_[symbol].end()->second.date_, "LONG");
+      bought_[symbol] = true;
+    }
+  }
+}
 
 } // namespace HackTest
--- ht/Strategy/SimpleStrategy.hpp
+++ ht/Strategy/SimpleStrategy.hpp
@@ -5,13 +5,22 @@
 #ifndef HACKTEST_SIMPLESTRATEGY_HPP
 #define HACKTEST_SIMPLESTRATEGY_HPP
 
+#include "ht/DataHandler/HistoricalCsvHandler.hpp"
+#include "ht/Event/Event.hpp"
 #include "ht/Strategy/Strategy.hpp"
 
 namespace HackTest {
 
 class SimpleStrategy : public Strategy {
 public:
+  SimpleStrategy(EventQueue &, HistoricalCsvHandler &);
   void calculate_signal(MarketEvent &) override;
+
+private:
+  EventQueue &q_;
+  HistoricalCsvHandler &dh_;
+  std::vector<std::string> symbols_;
+  std::map<std::string, bool> bought_;
 };
 
 } // namespace HackTest
